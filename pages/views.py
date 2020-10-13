9775165e02413f67a78ccb6b3661e98a301b62a6
Walther Seidel <waseidel@gmail.com>
2020-10-13 12:22:32 -0500

    first template

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 # pages/views.py
-from django.http import HttpResponse
+
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
